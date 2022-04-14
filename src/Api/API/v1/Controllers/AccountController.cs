bbc2d1c6eae34184d02f8537161ef0acbfbda7ec
serd2011 <45919738+serd2011@users.noreply.github.com>
2022-04-14 19:56:20 +0000

    Реализован контроллер аккаунта

--- src/Api/API/v1/Controllers/AccountController.cs
+++ src/Api/API/v1/Controllers/AccountController.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 using API.v1.Models.Account;
 using API.v1.Models;
+using API.Infrastructure.Database;
 
 namespace API.v1.Controllers
 {
@@ -10,8 +12,8 @@ namespace API.v1.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
-        private readonly Infrastructure.Database.Context _dbContext;
-        public AccountController(Infrastructure.Database.Context context)
+        private readonly Context _dbContext;
+        public AccountController(Context context)
         {
             _dbContext = context;
         }
@@ -19,13 +21,24 @@ namespace API.v1.Controllers
         [HttpGet]
         public IActionResult Get()
         {
-            return new OkObjectResult(new GetResponse() { balance = 10 });
+            var user = _dbContext.Users.Find(int.Parse(User.FindFirst("id").Value));
+            if(user == null)
+            {
+                user = new User() { Id = int.Parse(User.FindFirst("id").Value) };
+                _dbContext.Users.Add(user);
+                _dbContext.SaveChanges();
+            }
+            return new OkObjectResult(new GetResponse() { balance = (uint)user.Balance });
         }
 
+        [Authorize(Policy = "AdminOnly")]
         [HttpGet("{userId:int}")]
         public IActionResult Get(int userId)
         {
-            return new BadRequestObjectResult(new ErrorResponseModel() { code = 1, description = "There is no user with id " + userId.ToString() });
+            var user = _dbContext.Users.Find(userId);
+            if (user == null)            
+                return new BadRequestObjectResult(new ErrorResponseModel() { code = 1, description = "There is no user with id " + userId.ToString() });
+            return new OkObjectResult(new GetResponse() { balance = (uint)user.Balance });
         }
     }
 }
